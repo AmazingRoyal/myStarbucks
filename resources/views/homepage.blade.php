84626a96a87004b10ebb34318db60b395a415ae9
FreeFlow16 <FreeFlow16@users.noreply.github.com>
2021-07-05 21:48:33 +0700

    Update menu 4-6

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -9,8 +9,8 @@
                 <h1 class="header-nama">Mocha Frrappuchino</h1>
                 <h2 class="header-harga">Rp 47.000</h2>
                 <p class="header-deskripsi">
-                    For serious chocolate lovers: We blend dark cocoa with milk, 
-                    ice and coffee for an extraordinarily chocolatey experience 
+                    For serious chocolate lovers: We blend dark cocoa with milk,
+                    ice and coffee for an extraordinarily chocolatey experience
                     that's then topped with a swirl of whipped cream.
                 </p>
             </div>
@@ -57,31 +57,31 @@
                 <div class="row">
                     <div class="col home-menu">
                         <div class="card" style="width: 18rem;">
-                            <img src="{{ asset('/menu-1.png') }}" class="card-img-top" alt="...">
+                            <img src="{{ asset('/menu-4.png') }}" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <p class="card-menu">Mocha Frrappuchino</p>
-                                <p class="card-text">Blended Coffee</p>
-                                <p class="card-harga">Rp 47.000</p>
+                                <p class="card-menu">Iced Caramel Macchiato 57k</p>
+                                <p class="card-text">Brewed Coffee</p>
+                                <p class="card-harga">Rp 57.000</p>
                             </div>
                         </div>
                     </div>
                     <div class="col home-menu">
                         <div class="card" style="width: 18rem;">
-                            <img src="{{ asset('/menu-2.png') }}" class="card-img-top" alt="...">
+                            <img src="{{ asset('/menu-5.png') }}" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <p class="card-menu">Mocha Frrappuchino</p>
+                                <p class="card-menu">Caramel Frappucino</p>
                                 <p class="card-text">Blended Coffee</p>
-                                <p class="card-harga">Rp 47.000</p>
+                                <p class="card-harga">Rp 48.000</p>
                             </div>
                         </div>
                     </div>
                     <div class="col home-menu">
                         <div class="card" style="width: 18rem;">
-                            <img src="{{ asset('/menu-3.png') }}" class="card-img-top" alt="...">
+                            <img src="{{ asset('/menu-6.png') }}" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <p class="card-menu">Mocha Frrappuchino</p>
+                                <p class="card-menu">Iced Caffe Mocha</p>
                                 <p class="card-text">Blended Coffee</p>
-                                <p class="card-harga">Rp 47.000</p>
+                                <p class="card-harga">Rp 53.000</p>
                             </div>
                         </div>
                     </div>
